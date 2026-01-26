b7bd5fcadb49c75c644e095e7596eeacfcab79b3
Andreas Grech <agnt666@gmail.com>
2026-01-26 17:16:08 +0100

    old code in CSPCompatibilityManager

--- CSPCompatibilityManager.lua
+++ CSPCompatibilityManager.lua
@@ -16,12 +16,10 @@ local ADD_NON_EXISTANT_FUNCTIONS_TO_TEST_MISSING = false
 ---@param elementFn function
 ---@param elementName string
 ---@return TableForUsedElement
--- local getTableForUsedElement = function(elementFn, elementName, namespace)
 local getTableForUsedElement = function(elementFn, elementName)
     return {
         name = elementName,
         elementFn = elementFn,
-        -- namespace = namespace,
     }
 end
 
@@ -49,6 +47,9 @@ end
 
 local usedElements = {}
 
+---Add a function to be checked for existence in Custom Shaders Patch (CSP)
+---@param fn function
+---@param name string
 CSPCompatibilityManager.addFunction = function(fn, name)
     local tableForUsedElement = getTableForUsedElement(fn, name)
     table.insert(usedElements, tableForUsedElement)
@@ -58,74 +59,6 @@ local checkForMissingCSPElements = function()
     -- bindings (need to be in here for this class since we need to check for their existence)
     local ac = ac
     local ui = ui
-    -- local physics = physics
-
-    ---@type table<TableForUsedElement>
-    -- local usedACElements = {
-        -- getTableForUsedElement(function() return ac.log end, "ac.log"),
-        -- getTableForUsedElement(function() return ac.warn end, "ac.warn"),
-        -- getTableForUsedElement(function() return ac.error end, "ac.error"),
-        -- getTableForUsedElement(function() return ac.getSim end, "ac.getSim"),
-        -- getTableForUsedElement(function() return ac.storage end, "ac.storage"),
-        -- getTableForUsedElement(function() return ac.setWindowOpen end, "ac.setWindowOpen"),
-    -- }
-
-    -- ---@type table<TableForUsedElement>
-    -- local usedUIElements = {
-        -- getTableForUsedElement(function() return ui.button end, "ui.button"),
-        -- getTableForUsedElement(function() return ui.newLine end, "ui.newLine"),
-        -- getTableForUsedElement(function() return ui.text end, "ui.text"),
-        -- getTableForUsedElement(function() return ui.pushItemWidth end, "ui.pushItemWidth"),
-        -- getTableForUsedElement(function() return ui.popItemWidth end, "ui.popItemWidth"),
-        -- getTableForUsedElement(function() return ui.itemHovered end, "ui.itemHovered"),
-        -- getTableForUsedElement(function() return ui.setTooltip end, "ui.setTooltip"),
-        -- getTableForUsedElement(function() return ui.pushDisabled end, "ui.pushDisabled"),
-        -- getTableForUsedElement(function() return ui.popDisabled end, "ui.popDisabled"),
-        -- getTableForUsedElement(function() return ui.columns end, "ui.columns"),
-        -- getTableForUsedElement(function() return ui.ButtonFlags end, "ui.buttonFlags"),
-        -- getTableForUsedElement(function() return ui.pushStyleColor end, "ui.pushStyleColor"),
-        -- getTableForUsedElement(function() return ui.popStyleColor end, "ui.popStyleColor"),
-        -- getTableForUsedElement(function() return ui.textColored end, "ui.textColored"),
-        -- getTableForUsedElement(function() return ui.setColumnWidth end, "ui.setColumnWidth"),
-        -- getTableForUsedElement(function() return ui.separator end, "ui.separator"),
-        -- getTableForUsedElement(function() return ui.nextColumn end, "ui.nextColumn"),
-        -- getTableForUsedElement(function() return ui.pushID end, "ui.pushID"),
-        -- getTableForUsedElement(function() return ui.popID end, "ui.popID"),
-        -- getTableForUsedElement(function() return ui.itemClicked end, "ui.itemClicked"),
-        -- getTableForUsedElement(function() return ui.sameLine end, "ui.sameLine"),
-        -- getTableForUsedElement(function() return ui.slider end, "ui.slider"),
-        -- getTableForUsedElement(function() return ui.dwriteText end, "ui.dwriteText"),
-        -- getTableForUsedElement(function() return ui.checkbox end, "ui.checkbox"),
-        -- getTableForUsedElement(function() return ui.MouseButton end, "ui.MouseButton"),
-        -- getTableForUsedElement(function() return ui.mouseClicked end, "ui.mouseClicked"),
-        -- getTableForUsedElement(function() return ui.alignTextToFramePadding end, "ui.alignTextToFramePadding"),
-        -- getTableForUsedElement(function() return ui.setMouseCursor end, "ui.setMouseCursor"),
-        -- getTableForUsedElement(function() return ui.styleColor end, "ui.styleColor"),
-        -- getTableForUsedElement(function() return ui.StyleColor end, "ui.StyleColor"),
-    -- }
-
-    -- ---@type table<TableForUsedElement>
-    -- local usedPhysicsElements = {
-    --     getTableForUsedElement(function() return physics.overrideRacingFlag end, "physics.overrideRacingFlag"),
-    --     getTableForUsedElement(function() return physics.setAIThrottleLimit end, "physics.setAIThrottleLimit"),
-    --     getTableForUsedElement(function() return physics.setAITopSpeed end, "physics.setAITopSpeed"),
-    --     getTableForUsedElement(function() return physics.setAICaution end, "physics.setAICaution"),
-    --     getTableForUsedElement(function() return physics.setAIAggression end, "physics.setAIAggression"),
-    --     getTableForUsedElement(function() return physics.setAIStopCounter end, "physics.setAIStopCounter"),
-    --     getTableForUsedElement(function() return physics.setExtraAIGrip end, "physics.setExtraAIGrip"),
-    --     getTableForUsedElement(function() return physics.disableCarCollisions end, "physics.disableCarCollisions"),
-    --     getTableForUsedElement(function() return physics.setGentleStop end, "physics.setGentleStop"),
-    --     getTableForUsedElement(function() return physics.preventAIFromRetiring end, "physics.preventAIFromRetiring"),
-    --     getTableForUsedElement(function() return physics.setAISplineOffset end, "physics.setAISplineOffset"),
-    --     getTableForUsedElement(function() return physics.overrideRacingFlag end, "physics.overrideRacingFlag"),
-    --     -- getTableForUsedElement(function() return physics.setEngineStallEnabled end, "physics.setEngineStallEnabled"),
-    --     -- getTableForUsedElement(function() return physics.setCarPosition end, "physics.setCarPosition"),
-    --     -- getTableForUsedElement(function() return physics.setCarFuel end, "physics.setCarFuel"),
-    --     -- getTableForUsedElement(function() return physics.engageGear end, "physics.engageGear"),
-    --     -- getTableForUsedElement(function() return physics.setEngineRPM end, "physics.setEngineRPM"),
-    --     -- getTableForUsedElement(function() return physics.awakeCar end, "physics.awakeCar"),
-    --     -- getTableForUsedElement(function() return physics.setAINoInput end, "physics.setAINoInput"),
-    -- }
 
     -- Make sure we have access to the ac.getSim or ac.getSimState functions!
     ---@type table<TableForUsedElement>
@@ -142,13 +75,9 @@ local checkForMissingCSPElements = function()
 
     -- For testing: add some non-existant functions to see if the missing check works
     if ADD_NON_EXISTANT_FUNCTIONS_TO_TEST_MISSING then
-        -- table.insert(usedACElements, getTableForUsedElement(function() return ac.nonExistantFunction end, "ac.nonExistantFunction"))
-        -- table.insert(usedUIElements, getTableForUsedElement(function() return ui.nonExistantFunction end, "ui.nonExistantFunction"))
         table.insert(usedElements, getTableForUsedElement(function() return ac.nonExistantFunction end, "ac.nonExistantFunction"))
         table.insert(usedElements, getTableForUsedElement(function() return ui.nonExistantFunction end, "ui.nonExistantFunction"))
-        -- table.insert(usedPhysicsElements, getTableForUsedElement(function() return physics.nonExistantFunction end, "physics.nonExistantFunction"))
         if simStateFnAvailable then
-            -- table.insert(usedAcStateSimElements, getTableForUsedElement(function() return sim.nonExistantFunction end, "ac.getSim().nonExistantFunction"))
             table.insert(usedElements, getTableForUsedElement(function() return sim.nonExistantFunction end, "ac.getSim().nonExistantFunction"))
         end
     end
@@ -156,8 +85,6 @@ local checkForMissingCSPElements = function()
     ---Goes through the list of used elements and checks if any are not available
     ---@param elements table<TableForUsedElement>
     ---@param missingElementsNames table<string>
-    -- ---@param namespace string
-    -- local checkMissingElements = function(elements, missingElementsNames, namespace)
     local checkMissingElements = function(elements, missingElementsNames)
         for _, usedElement in ipairs(elements) do
             local elementFn = usedElement.elementFn
@@ -174,8 +101,6 @@ local checkForMissingCSPElements = function()
                 -- add the missing element name metadata to the list of missing elements
                 table.insert(missingElementsNames, usedElement.name)
                 if LOG_MISSING_ELEMENTS_WHILE_CHECKING then
-                    -- namespace = usedElement.namespace or namespace
-                    -- ac.log(string.format("[CSPCompatibilityManager] %s function '%s' is not available (nil)", namespace, usedElement.name))
                     ac.log(string.format("[CSPCompatibilityManager] function '%s' is not available (nil)", usedElement.name))
                 end
             end
@@ -185,24 +110,10 @@ local checkForMissingCSPElements = function()
     ---@type table<string>
     local missingElementsNames = {}
 
-    -------------
     checkMissingElements(usedElements, missingElementsNames)
-    -------------
-
-    -- Check for missing elements in ac
-    -- checkMissingElements(usedACElements, missingElementsNames, "ac")
-    -- checkMissingElements(usedACElements, missingElementsNames)
-
-    -- Check for missing elements in ui
-    -- checkMissingElements(usedUIElements, missingElementsNames, "ui")
-    -- checkMissingElements(usedUIElements, missingElementsNames)
-
-    -- -- Check for missing elements in physics
-    -- checkMissingElements(usedPhysicsElements, missingElementsNames, "physics")
 
     -- Check for missing elements in ac.getSim()
     if simStateFnAvailable then
-        -- checkMissingElements(usedAcStateSimElements, missingElementsNames, "ac.getSim()")
         checkMissingElements(usedAcStateSimElements, missingElementsNames)
     end
 
@@ -245,8 +156,6 @@ CSPCompatibilityManager.checkAndAlert = function(appName, appVersion)
     -- Check if any CSP elements used by the app are missing
     local missingCSPElements = checkForMissingCSPElements()
     local anyMissingCSPElements = (#missingCSPElements > 0)
-    -- ac.log(string.format("[CSPCompatibilityManager] Missing CSP elements: %s", table.concat(missingCSPElements, ", ")))
-    -- ac.log(string.format("[CSPCompatibilityManager] anyMissingCSPElements: %s", tostring(anyMissingCSPElements)))
     local missingCSPElementsErrorMessage
 
     -- Show an error modal dialog if any CSP elements are missing
